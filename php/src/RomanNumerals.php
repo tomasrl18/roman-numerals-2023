8484fbf6694bb4595ae5e746de5b8cf3d5298bf9
tomasrl18 <tomaslopezraigal@gmail.com>
2023-10-01 16:10:45 +0200

    RED: given_2_then_return_II

--- php/src/RomanNumerals.php
+++ php/src/RomanNumerals.php
@@ -6,22 +6,6 @@ class RomanNumerals
 {
     public function convertRomanNumeral(int $number): string
     {
-        $roman = "";
-
-        if ($number >= 10) {
-            $roman .= 'X';
-            $number -= 10;
-        }
-
-        if ($number >= 5) {
-            $roman .= 'V';
-            $number -= 5;
-        }
-
-        for ($i = 0; $i < $number; $i++) {
-            $roman .= "I";
-        }
-
-        return $roman;
+        return 'I';
     }
 }
--- php/tests/MyClassTest.php
+++ php/tests/MyClassTest.php
@@ -11,12 +11,7 @@ class MyClassTest extends TestCase
         return [
             'given_1_then_return_I' => [1, 'I'],
             'given_2_then_return_II' => [2, 'II'],
-            'given_3_then_return_III' => [3, 'III'],
-            'given_5_then_return_V' => [5, 'V'],
-            'given_7_then_return_VII' => [7, 'VII'],
-            'given_10_then_return_X' => [10, 'X'],
-            'given_12_then_return_XII' => [12, 'XII'],
-            'given_18_then_return_XVIII' => [18, 'XVIII']
+            //'given_3_then_return_III' => [3, 'III']
         ];
     }
 
