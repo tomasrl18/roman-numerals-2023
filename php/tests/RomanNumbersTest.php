230d153864b0a95462244e8dc38a60f50af04ad7
FuenSansano <fuensanosano@gmail.com>
2023-10-01 16:32:03 +0200

    RED: given_5_then_return_V

--- php/tests/RomanNumbersTest.php
+++ php/tests/RomanNumbersTest.php
@@ -13,6 +13,7 @@ class RomanNumbersTest extends TestCase
             'given_2_then_return_II' => [2, 'II'],
             'given_3_then_return_III' => [3, 'III'],
             'given_4_then_return_IV' => [4, 'IV'],
+            'given_5_then_return_V' => [5, 'V'],
         ];
     }
 
